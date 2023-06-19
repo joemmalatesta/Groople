9c460c3aee0a85891a8ffb129e6f56874dcd011c
joemmalatesta <joemmalatesta@gmail.com>
2023-06-18 20:35:22 -0400

    make answers reasonable length

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,9 +1,11 @@
 import { OPENAI_KEY } from '$env/static/private';
 import { Configuration, OpenAIApi } from 'openai';
 import type { Actions } from '@sveltejs/kit';
-const openAI = new OpenAIApi( new Configuration({
-      apiKey: OPENAI_KEY
-    }))
+const openAI = new OpenAIApi(
+	new Configuration({
+		apiKey: OPENAI_KEY
+	})
+);
 
 export const actions: Actions = {
 	default: async (event) => {
@@ -17,11 +19,10 @@ export const actions: Actions = {
 		const [letterObject] = dataArray.splice(letterIndex, 1);
 		const letter = letterObject?.value; // Use optional chaining in case letterObject is undefined
 
-
 		// Display the formatted data in the console
 		let useableData = dataArray
-  .map(({ index, value }) => `${index}: ${value ? value : 'ZZZZZZ'}`)
-  .join('\n');
+			.map(({ index, value }) => `${index}: ${value && String(value).length < 60 ? value : 'ZZZZZZ'}`)
+			.join('\n');
 		let prompt = `Welcome to Scattergories! You are the judge, and your task is to determine whether the player's responses match the category and start with the selected letter.
 		If the response is a valid match, please respond with a "Yes." If the response does not match or is a combination of just the letter and another word, please respond with a "No." 
 		The response should not be vague and should not unjustly use descriptive words to cheat the system. You should also respond "No" to answers that seem made up or random - They should be somewhat well known and something a group of players would agree on.
@@ -47,22 +48,24 @@ export const actions: Actions = {
 		Yes/No
 		
 		There should be no other words than yes or no. No explanation, no filling in the blanks, nothing more than 12 simple Yes or No's.
-		`
+		`;
 
-    const res = await openAI.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages: [{
-        role: "user",
-        content: prompt
-      }]
-    })
-	const answerArray = res.data.choices[0].message?.content
-	console.log(prompt)
-	console.log(answerArray)
-    return {
-		// Useable Data should be dataArray it makes more sense but whatever
-	  input : useableData,		
-      output: answerArray
-    }
+		const res = await openAI.createChatCompletion({
+			model: 'gpt-3.5-turbo',
+			messages: [
+				{
+					role: 'user',
+					content: prompt
+				}
+			]
+		});
+		const answerArray = res.data.choices[0].message?.content;
+		console.log(prompt);
+		console.log(answerArray);
+		return {
+			// Useable Data should be dataArray it makes more sense but whatever
+			input: useableData,
+			output: answerArray
+		};
 	}
 };
