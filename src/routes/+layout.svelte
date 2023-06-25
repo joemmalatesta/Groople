1eee774d260dfc88ebfc6d886898f1679ea83fba
joemmalatesta <joemmalatesta@gmail.com>
2023-06-24 20:53:47 -0400

    edge config?

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,17 @@
 <script>
-    import "../app.css";
-  </script>
-  
-  <div class="flex justify-center items-center flex-col">
-  <a
-    href="/"
+	import '../app.css';
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+
+	inject({ mode: dev ? 'development' : 'production' });
+</script>
+
+<div class="flex justify-center items-center flex-col">
+	<a
+		href="/"
 		class="md:text-7xl text-5xl font-semibold text-transparent bg-black bg-clip-text p-3 flex items-center"
 	>
 		<img src="svelteLogo.png" alt="Svelte Logo" class="md:w-16 w-12 mr-1" />cattergories
 	</a>
 </div>
-  <slot />
+<slot />
