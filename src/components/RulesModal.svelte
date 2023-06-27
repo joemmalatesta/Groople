5eb982f74ad07cdc49c77df6a856838f53c3866f
joemmalatesta <joemmalatesta@gmail.com>
2023-06-26 23:43:45 -0400

    small change to rules

--- src/components/RulesModal.svelte
+++ src/components/RulesModal.svelte
@@ -35,7 +35,7 @@
 		{#each rules as rule, index}
 			<h4 class="md:text-xl text-sm md:font-semibold">• {rule.rule}</h4>
 			<button
-				class="underline underline-offset-2 hover:underline-offset-4 transition-all"
+				class="md:text-base text-xs underline underline-offset-2 hover:underline-offset-4 transition-all"
 				title="Show Examples"
 				on:click={() => {
 					showExamples[index] = !showExamples[index];
@@ -47,7 +47,7 @@
 				{#key showExamples[index]}
 					<ul transition:slide>
 						{#each rule.examples as example}
-							<li class="text-sm md:text-md">{example}</li>
+							<li class="text-sm md:text-base">{example}</li>
 						{/each}
 					</ul>
 				{/key}
