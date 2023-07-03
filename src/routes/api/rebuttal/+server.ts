c15c87316471030912708c4cc84178912f053a47
joemmalatesta <joemmalatesta@gmail.com>
2023-07-03 00:40:30 -0400

    rework rebuttal for better or for worse

--- src/routes/api/rebuttal/+server.ts
+++ src/routes/api/rebuttal/+server.ts
@@ -12,9 +12,7 @@ const openAI = new OpenAIApi(
 export const POST = (async ({ request }) => {
     try {
       const { category, recordedAnswer } = await request.json();
-      let prompt = `You're the judge for the game Scattergories and you've been asked to reassess your answer.
-      The player answered: ${recordedAnswer} for the category: ${category} 
-      If you think this is a valid answer, reply with "Yes" and nothing more. Otherwise, reply with "No" and nothing more. There should be no other words in your response besides Yes or No.`;
+      let prompt = `Does the response "${recordedAnswer}" fit in the category "${category}"? There should be no other words in your response besides Yes or No, no explanation should be provided`;
       const res = await openAI.createChatCompletion({
         model: 'gpt-3.5-turbo',
         messages: [
@@ -25,13 +23,20 @@ export const POST = (async ({ request }) => {
         ],
       });
 
-      
-      const response = res.data.choices[0].message?.content;
+      // Eventually remove one from uses.
+
+      let response = res.data.choices[0].message?.content.replace(/\./g, '');
+      console.log(`Rebuttal: ${prompt}\nResponse: ${response}`);
+      //Default to no if it gives some stupid shit
+      if (response?.toLowerCase() != "yes" && response?.toLowerCase() != "no"){
+        console.log("shit response... Rebuttal broke")
+        response = "no"
+      }
       if (response === undefined) {
         throw new Error('Failed to retrieve valid response from the AI model.');
       }
   
-      console.log(`Rebuttal: ${prompt}\nResponse: ${response}`);
+      
       return json(response.toLowerCase());
     } catch (error) {
       console.error(error);
