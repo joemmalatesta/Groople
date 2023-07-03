61f59d9255b83ada5a1a6b57ce3efaacb8911811
joemmalatesta <joemmalatesta@gmail.com>
2023-07-03 00:39:03 -0400

    we are now groople

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,7 +11,8 @@
 		href="/"
 		class="md:text-7xl text-5xl font-semibold text-transparent bg-black bg-clip-text p-3 flex items-center"
 	>
-		<img src="svelteLogo.png" alt="Svelte Logo" class="md:w-16 w-12 mr-1" />cattergories
+		<!-- <img src="svelteLogo.png" alt="Svelte Logo" class="md:w-16 w-12 mr-1" /> -->
+		Groople
 	</a>
 </div>
 <slot />
