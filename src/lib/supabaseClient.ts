2124de8fd809c97f8afc147cdb540d59b77a88e3
joemmalatesta <joemmalatesta@gmail.com>
2023-07-06 21:24:51 -0400

    hide da URL oops.

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,4 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
-import { SUPABASE_SECRET} from '$env/static/private'
+import { SUPABASE_SECRET, SUPABASE_URL} from '$env/static/private'
 
-export const supabase = createClient('https://ipnppjfncsoflyoqyoxw.supabase.co', SUPABASE_SECRET)
+export const supabase = createClient(SUPABASE_URL, SUPABASE_SECRET)
