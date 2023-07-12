b31ec2061898b37dc710dfab2822e91342a8689a
Joe Malatesta <joemmalatesta@gmail.com>
2023-07-12 05:46:18 -0400

    update rules modal with rebuttal.

--- src/components/RulesModal.svelte
+++ src/components/RulesModal.svelte
@@ -11,10 +11,6 @@
 			category: 'Car parts',
 			answer: 'Windows'
 		},
-		{
-			category: 'Fruits',
-			answer: ''
-		}
 	];
 
 	export let modalActive: boolean = true;
@@ -31,7 +27,6 @@
 	<p class="flex flex-col items-start text-base md:text-xl mb-2">
 		For each category, find a word that fits the category and starts with "{letter}"
 	</p>
-	<p class="text-center underline underline-offset-2 ">Try these</p>
 
 	<!-- Do something about the rebuttal rule here as well -->
 	{#each examples as example, index}
@@ -47,8 +42,10 @@
 			/>
 		</div>
 	{/each}
-	<div class="flex flex-col items-center text-sm md:text-xl mt-2">
+	<div class="flex flex-col items-center mx-20 text-sm md:text-xl mt-2">
 		<p>Answers are judged by AI and either given a ✔️ or ❌</p>
+		<p class="gap-2 flex items-center">For incorrect answers, you can <span class="px-2 py-0.5 bg-neutral-600 hover:bg-neutral-700 rounded-3xl text-white text-sm cursor-pointer" >Rebuttal</span> the AI
+		</p>
 		<p>You have 100 seconds to answer 12 categories</p>
 	</div>
 	<div class="flex justify-center items-center m-3">
