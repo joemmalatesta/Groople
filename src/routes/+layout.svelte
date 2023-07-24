683afd7d310e595be2b3c4fead3e3646d9373368
joemmalatesta <joemmalatesta@gmail.com>
2023-07-24 01:02:09 -0400

    new icon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,7 +8,9 @@
 
 <header class="flex justify-center items-center flex-col relative">
 	<div class="absolute md:left-20 left-2">
-		<img src="favicon.svg" alt="Groople Logo" class="md:w-20 w-16 mt-2" />
+		<a href="/">
+			<img src="favicon.svg" alt="Groople Logo" class="md:w-20 w-16 mt-2" />
+		</a>
 	</div>
 	<h1>
 		<a
