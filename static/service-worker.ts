7d890cb8d99b1a0ea20d34a16e02f729375bf9ef
joemmalatesta <joemmalatesta@gmail.com>
2023-07-25 18:57:23 -0400

    hopedully this is fix

--- static/service-worker.ts
+++
@@ -1,8 +0,0 @@
-if ("serviceWorker" in navigator){
-    navigator.serviceWorker.register("../sw.js").then(registration => {
-        console.log("ServiceWorker Registered");
-        console.log(registration)
-    }).catch(error => {
-        console.log(`ServiceWorker registration failed error: ${error}`)
-    })
-}
