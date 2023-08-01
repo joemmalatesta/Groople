2fc835ea07805e67663de28d12b66845ff0753e1
joemmalatesta <joemmalatesta@gmail.com>
2023-07-31 23:19:25 -0400

    login function added

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,23 +1,28 @@
-import { createClient } from '@supabase/supabase-js'
-import { SUPABASE_SECRET, SUPABASE_URL,  SUPABASE_CLIENT_KEY} from '$env/static/private'
-import { redirect } from '@sveltejs/kit'
+import { createClient } from '@supabase/supabase-js';
+import { SUPABASE_SECRET, SUPABASE_URL, SUPABASE_CLIENT_KEY } from '$env/static/private';
+import { redirect } from '@sveltejs/kit';
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_SECRET)
+export const supabase = createClient(SUPABASE_URL, SUPABASE_SECRET);
 
 export async function loginWithGoogle() {
-    console.log("this running?")
-    const { data, error } = await supabase.auth.signInWithOAuth({
-        provider: 'google',
-    })
-    console.log(data.url)
-    supabase.auth.onAuthStateChange((event, session) => {
-        console.log(event, session)
-    });
-    throw redirect (303, String(data.url))
-    
+	console.log('this running?');
+	const { data, error } = await supabase.auth.signInWithOAuth({
+		provider: 'google'
+	});
+	console.log(data.url);
+	supabase.auth.onAuthStateChange((event, session) => {
+		console.log(event, session);
+	});
+	throw redirect(303, String(data.url));
 }
 
+async function signInWithEmail() {
+	const { data, error } = await supabase.auth.signInWithPassword({
+		email: 'example@email.com',
+		password: 'example-password'
+	});
+}
 
-export function logout() {
-
+async function signOut() {
+	const { error } = await supabase.auth.signOut();
 }
