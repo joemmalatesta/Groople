17b43829b604e4e1dcba2b48a7972a04f2a6799a
Joe <joemmalatesta@gmail.com>
2024-06-03 16:05:06 -0700

    adding tracing to hooks

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,7 +3,8 @@ import crypto from 'crypto';
 import type { HandleServerError } from '@sveltejs/kit';
 
 Sentry.init({
-	dsn: 'https://75d2773c27d883dfaf3775ff83b73913@o4507370066870272.ingest.us.sentry.io/4507370067722240'
+	dsn: 'https://75d2773c27d883dfaf3775ff83b73913@o4507370066870272.ingest.us.sentry.io/4507370067722240',
+	tracesSampleRate: 1.0,
 });
 
 export const handleError: HandleServerError = async ({ error, event }) => {
