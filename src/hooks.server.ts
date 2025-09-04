3031c4d534854e5b2b56ca780b239ab06677efe3
Joe <joemmalatesta@gmail.com>
2025-09-03 23:03:57 -0700

    try deploy agai...

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,5 +2,5 @@ import type { HandleServerError } from '@sveltejs/kit';
 import { error as errorResponse } from '@sveltejs/kit';
 
 export const handleError: HandleServerError = ({ error, event }) => {
-	console.error("An error occurred on the server side:", error, event);
+	console.error('An error occurred on the server side:', error, event);
 };
--- svelte.config.js
+++ svelte.config.js
@@ -3,10 +3,12 @@ import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter()
-  },
-  preprocess: vitePreprocess()
+	kit: {
+		adapter: adapter({
+			runtime: 'nodejs18.x'
+		})
+	},
+	preprocess: vitePreprocess()
 };
 
 export default config;
